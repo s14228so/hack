540e5d8977f90837b5ed8683dc4e8fc32c386cc9
satoshi ono <saongtx7@gmail.com>
2019-02-13 23:44:15 +0900

    rails_api new

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
       protect_from_forgery with: :null_session 
-
+  before_action :authenticate_user_from_token!
+  
   def after_sign_in_path_for(resource)
     root_path
   end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_040813) do
+ActiveRecord::Schema.define(version: 2019_02_13_141414) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2019_02_09_040813) do
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "access_token"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end

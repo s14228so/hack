e3363ec572f7678b528384efa0e20fe65b9f6abc
satoshi ono <saongtx7@gmail.com>
2019-02-16 19:53:41 +0900

    railsふおるだ変更

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
-class ApplicationController < ActionController::Base
-      protect_from_forgery with: :null_session 
-  before_action :authenticate_user_from_token!
-  
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
-  def after_sign_out_path_for(resource)
-     login_path
-  end
+  private
 
+    def current_user
+      @current_user ||= User.find(payload['user_id'])
+    end
+
+    def not_authorized
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
 end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_141414) do
+ActiveRecord::Schema.define(version: 2019_02_16_063710) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -108,6 +108,13 @@ ActiveRecord::Schema.define(version: 2019_02_13_141414) do
   create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "student_team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

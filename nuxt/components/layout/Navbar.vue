3f3fd2177feef9a037bd857fede3e8e21a0e167c
satoshi ono <saongtx7@gmail.com>
2019-02-17 03:58:07 +0900

    navabr変えた

--- nuxt/components/layout/Navbar.vue
+++ nuxt/components/layout/Navbar.vue
@@ -1,38 +1,38 @@
 
  <template>
- <div>
-  <v-navigation-drawer fixed v-model="drawer" app class="blue-grey lighten-4">
-    <v-list dense>
-      <navbar-list v-for="tag in tags" :tag="tag" :login="login">></navbar-list>
-      <v-list-tile @click v-show="!login">
-        <v-list-tile-action>
-          <v-icon>open_in_new</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            <log-in></log-in>
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile @click v-show="login">
-        <v-list-tile-action>
-          <v-icon>exit_to_app</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            <log-out></log-out>
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
-  <v-toolbar class="blue-grey lighten-4 grey--text" fixed app>
-    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-    <v-toolbar-title>Hack</v-toolbar-title>
-  </v-toolbar>
-  <!-- <v-footer color="indigo" app>
+  <div>
+    <v-navigation-drawer fixed v-model="drawer" app class="blue-grey lighten-4">
+      <v-list dense>
+        <navbar-list v-for="tag in tags" :tag="tag" :login="login">></navbar-list>
+        <v-list-tile @click v-show="!login">
+          <v-list-tile-action>
+            <v-icon>open_in_new</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <log-in></log-in>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click v-show="login">
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <log-out></log-out>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="blue-grey lighten-4 grey--text" fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Hack</v-toolbar-title>
+    </v-toolbar>
+    <!-- <v-footer color="indigo" app>
       <span class="white--text">&copy; 2017</span>
-  </v-footer>-->
+    </v-footer>-->
   </div>
 </template>
 

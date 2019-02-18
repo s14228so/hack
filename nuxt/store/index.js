a78078600a1186c829e364b463ee1c1a8213d3fe
satoshi ono <saongtx7@gmail.com>
2019-02-19 02:05:45 +0900

    APIだけデプロイ

--- nuxt/store/index.js
+++ nuxt/store/index.js
@@ -26,29 +26,5 @@ const createStore = () => {
 
 export default createStore;
 
-// import Vuex from "vuex";
-
-// Vue.use(Vuex);
-
-// export default new Vuex.Store({
-//   state: () => ({
-//     login: true
-//   }),
-//   mutations: {
-//     login(state) {
-//       state.login = true;
-//       localStorage.setItem("login", JSON.stringify(state));
-//     },
-//     logout(state) {
-//       state.login = false;
-//       localStorage.setItem("login", JSON.stringify(state));
-//     }
-//   },
-//   plugins: [
-//     createPersistedState({
-//       key: "login",
-//       paths: ["login"],
-//       storage: window.sessionStorage
-//     })
-//   ]
-// });
+// Rails API
+// https://rails-api-for-nuxt.herokuapp.com/

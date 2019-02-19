0fa212b2f8034c86fa9c83d4bb599ca76fb3ac6c
satoshi ono <saongtx7@gmail.com>
2019-02-20 02:29:22 +0900

    emailを追加。serializer(student)

--- rails_api/app/serializers/v1/student_serializer.rb
+++ rails_api/app/serializers/v1/student_serializer.rb
@@ -1,6 +1,6 @@
 module V1
   class StudentSerializer < ActiveModel::Serializer
-    attributes :student_id, :access_token, :nickname, :images, :created_at, :app,:updated_at, :grade, :university, :department, :address, :phone_number, :first_name, :last_name, :introduction
+    attributes :student_id, :email, :access_token, :nickname, :images, :created_at, :app,:updated_at, :grade, :university, :department, :address, :phone_number, :first_name, :last_name, :introduction
     
     
     def student_id

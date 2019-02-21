6ff746e3bbc3c2477c90f533e29cd083ce50f697
satoshi ono <saongtx7@gmail.com>
2019-02-22 00:48:23 +0900

    indent

--- rails_api/app/serializers/v1/session_serializer.rb
+++ rails_api/app/serializers/v1/session_serializer.rb
@@ -2,7 +2,6 @@ module V1
   class SessionSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
 
-
     attributes :email, :nickname, :image, :university, :department,:grade,:first_name,:last_name,:app,:phone_number,:address, :token_type, :student_id, :access_token
 
     def student_id
--- rails_api/app/serializers/v1/student_serializer.rb
+++ rails_api/app/serializers/v1/student_serializer.rb
@@ -2,8 +2,7 @@ module V1
   class StudentSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
     attributes :student_id, :email, :access_token, :nickname, :image, :created_at, :app,:updated_at, :grade, :university, :department, :address, :phone_number, :first_name, :last_name, :introduction
-    
-    
+
     def student_id
       object.id
     end

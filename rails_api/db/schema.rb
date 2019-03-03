58461be31b25d4d16ec002b8605d055bb86799fa
satoshi ono <saongtx7@gmail.com>
2019-03-03 10:10:11 +0900

    電話番号をvercharに、validatorも追加

--- rails_api/db/schema.rb
+++ rails_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_220059) do
+ActiveRecord::Schema.define(version: 2019_03_03_010632) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_220059) do
     t.string "last_name"
     t.text "introduction"
     t.text "app"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

beb5f7bfc6f47be8176014cc25661e17c3abb07a
satoshi ono <saongtx7@gmail.com>
2019-03-17 00:45:27 +0900

    ProfileImageコンポーネント作成

---
+++ front/toC/components/my/ProfileImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form action method="post" novalidate="true" @submit.prevent="uploadImage" class="uploader">
+      <h3>プロフィール写真の追加</h3>
+      <hr>
+      <input
+        type="file"
+        name="student[images]"
+        id="student_image"
+        @change="selectedFile"
+        accept="image/*"
+        placeholder="Upload file..."
+      >
+      <br>
+      <input
+        type="submit"
+        name="commit"
+        value="追加"
+        data-disable-with="Update Student"
+        class="img-btn"
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

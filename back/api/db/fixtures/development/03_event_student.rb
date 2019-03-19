44e94376697d658983d63071d5bf31d76969efe0
satoshi ono <saongtx7@gmail.com>
2019-03-19 15:57:52 +0900

    03_event_student.rb

---
+++ back/api/db/fixtures/development/03_event_student.rb
@@ -0,0 +1,27 @@
+
+
+EventStudent.seed do |s|
+  s.id = 1
+  s.event_id = 1
+  s.student_id = 2
+end
+
+EventStudent.seed do |s|
+  s.id = 2
+  s.event_id = 1
+  s.student_id = 2
+end
+
+EventStudent.seed do |s|
+  s.id = 3
+  s.event_id = 1
+  s.student_id = 2
+end
+
+EventStudent.seed do |s|
+  s.id = 4
+  s.event_id = 1
+  s.student_id = 2
+end
+
+

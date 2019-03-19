14528cf005a781f65b41226a4359ddd987e57fc0
satoshi ono <saongtx7@gmail.com>
2019-03-19 13:12:49 +0900

    event詳細

--- front/toC/components/my/MyHacks.vue
+++ front/toC/components/my/MyHacks.vue
@@ -1,9 +1,16 @@
 <template>
   <v-container>
     <event-detail @rand="showDetail" :events="events"></event-detail>
-    <div v-show="detailStatus">
-      {{event.title}}
+    <div v-show="detailStatus" class="box">
+      <h4>{{event.title}}</h4>
+      <hr>
+      <p>{{event.content}}</p>
       <div v-for="student in students" class="mt-3" :key="student.email">
+        <div v-if="student.image">
+          <v-avatar>
+            <img :src="student.image">
+          </v-avatar>
+        </div>
         <v-chip @click="idPush(student)">{{ student.email }}</v-chip>
       </div>
     </div>
@@ -29,6 +36,7 @@ export default {
     EventDetail
   },
   mounted: async function() {
+    console.log(this.students);
     const res = await axios.get(`http://localhost:5000/v1/events/myhacks`, {
       headers: { Authorization: this.student.access_token }
     });
@@ -36,6 +44,7 @@ export default {
       process.exit();
     }
     this.events = res.data;
+    console.log(this.events);
   },
 
   methods: {
@@ -66,4 +75,19 @@ export default {
 };
 </script>
 <style scoped>
+.box {
+  width: 300px;
+  height: 400px;
+  padding: 20px;
+  border: 1px solid gray;
+  border-radius: 10px;
+}
+
+h4 {
+  font-size: 18px;
+}
+p {
+  font-size: 12px;
+  margin-top: 10px;
+}
 </style>

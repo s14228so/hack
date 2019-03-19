f49aecf26043535e44d460991062ba99dde18f4e
satoshi ono <saongtx7@gmail.com>
2019-03-19 13:52:05 +0900

    event_seed

--- back/api/db/fixtures/development/02_event.rb
+++ back/api/db/fixtures/development/02_event.rb
@@ -1,44 +1,49 @@
+    # t.string "title"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+    # t.text "content"
+    # t.string "date"
+  
+
 Event.seed do |s|
   s.id = 1
-  s.title = "Mondo"
-  s.email = "mondo@gmail.com"
-  s.password = "mondomondo"
-  s.password = "password"
+  s.title = "イベント1(仮)"
+  s.content = "イベント内容1"
+  s.date = "2019-03-01"
 end
 
 Event.seed do |s|
   s.id = 2
-  s.title = "Shingai"
-  s.email = "shingai@gmail.com"
-  s.password = "shingai"
-  s.password = "password"
+  s.title = "イベント2(仮)"
+  s.content = "イベント内容2"
+  s.date = "2019-03-02"
 end
 
 Event.seed do |s|
   s.id = 3
-  s.nickame = "Ono"
-  s.email = "ono@ono.com"
-  s.password = "password"
+  s.title = "イベント3(仮)"
+  s.content = "イベント内容3"
+  s.date = "2019-03-03"
 end
 
 Event.seed do |s|
   s.id = 4
-  s.title = "test"
-  s.email = "test@test.com"
-  s.password = "password"
+  s.title = "イベント4(仮)"
+  s.content = "イベント内容4"
+  s.date = "2019-03-04"
 end
 
 Event.seed do |s|
-  s.id = 4
-  s.title = "yamada"
-  s.email = "yamada@yamada.com"
-  s.password = "password"
+  s.id = 5
+  s.title = "イベント5(仮)"
+  s.content = "イベント内容5"
+  s.date = "2019-03-05"
 end
 
 
 Event.seed do |s|
-  s.id = 4
-  s.title = "tanaka"
-  s.email = "tanaka@tanaka.com"
-  s.password = "password"
+  s.id = 6
+  s.title = "イベント6(仮)"
+  s.content = "イベント内容6"
+  s.date = "2019-03-06"
 end

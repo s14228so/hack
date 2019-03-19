8766bcd1bf64f03eab4ba0d1f04ecf6d3e6a4797
satoshi ono <saongtx7@gmail.com>
2019-03-19 13:38:00 +0900

    add

---
+++ back/api/db/fixtures/development/01_student.rb
@@ -0,0 +1,36 @@
+Student.seed do |s|
+  s.id = 1
+  s.nickname = "Mondo"
+  s.email = "mondo@gmail.com"
+  s.password = "mondomondo"
+  s.password = "password"
+end
+
+Student.seed do |s|
+  s.id = 2
+  s.nickname = "Shingai"
+  s.email = "shingai@gmail.com"
+  s.password = "shingai"
+  s.password = "password"
+end
+
+Student.seed do |s|
+  s.id = 3
+  s.nickame = "Ono"
+  s.email = "ono@ono.com"
+  s.password = "password"
+end
+
+Student.seed do |s|
+  s.id = 4
+  s.nickname = "test"
+  s.email = "test@test.com"
+  s.password = "password"
+end
+
+Student.seed do |s|
+  s.id = 4
+  s.nickname = "yamada"
+  s.email = "test@test.com"
+  s.password = "password"
+end
